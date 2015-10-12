8ee1bb9c18cc2c1474b5b18e400590987ed47618
Geoff Simmons <geoff@uplex.de>
2015-10-12 07:29:45 +0200

    Revert "use PRIV_CALL private objects to create/destroy uuid_t objects only at" Not thread-safe This reverts commit 5ebde5935c0132d0a068da30bea722ec2bad3960.

--- src/vmod_uuid.c
+++ src/vmod_uuid.c
@@ -29,65 +29,75 @@
 #include <string.h>
 #include <uuid.h>
 #include <stdarg.h>
-#include <stdlib.h>
 
 #include "vrt.h"
 #include "cache/cache.h"
 
 #include "vcc_if.h"
 
-#define UUID_CALL(RC,CTX,CALL,RET)				\
+#define UUID_CALL(RC,CTX,CALL,UUID,UUIDNS)			\
 	do {							\
 		if (((RC) = (CALL)) != UUID_RC_OK) {		\
 			VSLb((CTX)->vsl, SLT_VCL_Error,		\
-			     "vmod uuid error %d: %s", (RC),	\
-			     uuid_error(RC));			\
-			return(RET);				\
+			    "vmod uuid error %d: %s", (RC),	\
+			    uuid_error(RC));			\
+			if ((UUID) != NULL)			\
+				uuid_destroy(UUID);		\
+			if ((UUIDNS) != NULL)			\
+				uuid_destroy(UUIDNS);		\
+			return(-1);				\
 		}						\
 	} while(0)
-
+   
 static inline int
-mkuuid(VRT_CTX, int utype, uuid_t *uuid, const char *str, va_list ap)
+mkuuid(const struct vrt_ctx *ctx, int utype, const char *str, va_list ap)
 {
-	uuid_t *uuid_ns = NULL;
+	uuid_t *uuid = NULL, *uuid_ns = NULL;
 	uuid_rc_t rc;
 	char *ns, *name;
 	size_t len = UUID_LEN_STR + 1;
 
 	if (utype == UUID_MAKE_V3 || utype == UUID_MAKE_V5) {
-		uuid_ns = (uuid_t *) va_arg(ap, uuid_t *);
-		AN(uuid_ns);
+		UUID_CALL(rc, ctx, uuid_create(&uuid_ns), uuid, uuid_ns);
 		ns = (char *) va_arg(ap, char *);
 		AN(ns);
 		name = (char *) va_arg(ap, char *);
 		AN(name);
 		if (uuid_load(uuid_ns, ns) != UUID_RC_OK
 		    && uuid_import(uuid_ns, UUID_FMT_STR, (const void *) ns,
-                                   strlen(ns)) != UUID_RC_OK)
+                                   strlen(ns)) != UUID_RC_OK) {
+			UUID_CALL(rc, ctx, uuid_destroy(uuid_ns), uuid,
+                                  uuid_ns);
 			return(-1);
+		}
 		AN(uuid_ns);
 	}
 
-	UUID_CALL(rc, ctx, uuid_make(uuid, utype, uuid_ns, name), -1);
-	UUID_CALL(rc, ctx, uuid_export(uuid, UUID_FMT_STR, &str, &len), -1);
+	UUID_CALL(rc, ctx, uuid_create(&uuid), uuid, uuid_ns);
+	UUID_CALL(rc, ctx, uuid_make(uuid, utype, uuid_ns, name), uuid,
+                  uuid_ns);
+	UUID_CALL(rc, ctx, uuid_export(uuid, UUID_FMT_STR, &str, &len), uuid,
+	          uuid_ns);
 	assert(len == UUID_LEN_STR + 1);
+	UUID_CALL(rc, ctx, uuid_destroy(uuid), uuid, uuid_ns);
+	if (uuid_ns != NULL)
+		uuid_destroy(uuid_ns);
 	return(0);
 }
 
 static VCL_STRING
-_uuid(VRT_CTX, uuid_t *uuid, int utype, ...)
+_uuid(const struct vrt_ctx *ctx, int utype, ...)
 {
 	char *p, uuid_str[UUID_LEN_STR + 1];
 	va_list ap;
 	int ret;
 
 	CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
-	AN(uuid);
 	assert(utype == UUID_MAKE_V1 || utype == UUID_MAKE_V3
 	    || utype == UUID_MAKE_V4 || utype == UUID_MAKE_V5);
 
 	va_start(ap, utype);
-	ret = mkuuid(ctx, utype, uuid, uuid_str, ap);
+	ret = mkuuid(ctx, utype, uuid_str, ap);
 	va_end(ap);
 	if (ret != 0)
 		return(NULL);
@@ -102,100 +112,32 @@ _uuid(VRT_CTX, uuid_t *uuid, int utype, ...)
 	return(p);
 }
 
-static void
-free1uuid(void *priv)
+VCL_STRING
+vmod_uuid_v1(const struct vrt_ctx *ctx)
 {
-	AN(priv);
-	uuid_destroy((uuid_t *) priv);
-}
-
-static inline uuid_t *
-get1uuid(VRT_CTX, struct vmod_priv *priv)
-{
-	AN(priv);
-	if (priv->priv == NULL) {
-		uuid_rc_t rc;
-		uuid_t *uuid = NULL;
-
-		CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
-		UUID_CALL(rc, ctx, uuid_create(&uuid), NULL);
-		priv->priv = uuid;
-		priv->free = free1uuid;
-	}
-	return (uuid_t *) priv->priv;
-}
-
-static void
-free2uuids(void *priv)
-{
-	uuid_t **p;
-
-	AN(priv);
-	p = priv;
-	uuid_destroy(p[0]);
-	uuid_destroy(p[1]);
-}
-
-static inline uuid_t *
-get2uuids(VRT_CTX, struct vmod_priv *priv, uuid_t **uuid_ns)
-{
-	uuid_t **uuids;
-
-	AN(priv);
-	AN(uuid_ns);
-	if (priv->priv == NULL) {
-		uuid_rc_t rc;
-		uuid_t *uuid = NULL;
-
-		CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
-		*uuid_ns = NULL;
-		UUID_CALL(rc, ctx, uuid_create(&uuid), NULL);
-		UUID_CALL(rc, ctx, uuid_create(uuid_ns), NULL);
-		priv->priv = malloc(2 * sizeof(uuid_t *));
-		AN(priv->priv);
-		uuids = (uuid_t **) priv->priv;
-		uuids[0] = uuid;
-		uuids[1] = *uuid_ns;
-		priv->free = free2uuids;
-		return uuid;
-	}
-	uuids = (uuid_t **) priv->priv;
-	*uuid_ns = uuids[1];
-	return uuids[0];
+	return _uuid(ctx, UUID_MAKE_V1);
 }
 
 VCL_STRING
-vmod_uuid_v1(VRT_CTX, struct vmod_priv *priv)
+vmod_uuid_v3(const struct vrt_ctx *ctx, VCL_STRING ns, VCL_STRING name)
 {
-	uuid_t *uuid = get1uuid(ctx, priv);
-	return _uuid(ctx, uuid, UUID_MAKE_V1);
+	return _uuid(ctx, UUID_MAKE_V3, ns, name);
 }
 
 VCL_STRING
-vmod_uuid_v3(VRT_CTX, struct vmod_priv *priv, VCL_STRING ns, VCL_STRING name)
+vmod_uuid_v4(const struct vrt_ctx *ctx)
 {
-	uuid_t *uuid_ns;
-	uuid_t *uuid = get2uuids(ctx, priv, &uuid_ns);
-	return _uuid(ctx, uuid, UUID_MAKE_V3, uuid_ns, ns, name);
+	return _uuid(ctx, UUID_MAKE_V4);
 }
 
 VCL_STRING
-vmod_uuid_v4(VRT_CTX, struct vmod_priv *priv)
+vmod_uuid_v5(const struct vrt_ctx *ctx, VCL_STRING ns, VCL_STRING name)
 {
-	uuid_t *uuid = get1uuid(ctx, priv);
-	return _uuid(ctx, uuid, UUID_MAKE_V4);
+	return _uuid(ctx, UUID_MAKE_V5, ns, name);
 }
 
 VCL_STRING
-vmod_uuid_v5(VRT_CTX, struct vmod_priv *priv, VCL_STRING ns, VCL_STRING name)
+vmod_uuid(const struct vrt_ctx *ctx)
 {
-	uuid_t *uuid_ns;
-	uuid_t *uuid = get2uuids(ctx, priv, &uuid_ns);
-	return _uuid(ctx, uuid, UUID_MAKE_V5, uuid_ns, ns, name);
-}
-
-VCL_STRING
-vmod_uuid(VRT_CTX, struct vmod_priv *priv)
-{
-	return vmod_uuid_v1(ctx, priv);
+	return vmod_uuid_v1(ctx);
 }
